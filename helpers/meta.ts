85dbacd574f99df30998056cf3bc91cc4ad3f11a
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-26 10:45:37 +0100

    :heavy_plus_sign: Add got

--- helpers/meta.ts
+++ helpers/meta.ts
@@ -1,5 +1,20 @@
 import metascraper from "metascraper";
+import author from "metascraper-author";
+import date from "metascraper-date";
+import description from "metascraper-description";
+import publisher from "metascraper-publisher";
+import title from "metascraper-title";
+import url from "metascraper-url";
+import lang from "metascraper-lang";
 
-const scraper = metascraper();
+const scraper = metascraper([
+  author(),
+  date(),
+  description(),
+  publisher(),
+  title(),
+  url(),
+  lang()
+]);
 
 export const scrapeMetaTags = async (url: string) => {};
