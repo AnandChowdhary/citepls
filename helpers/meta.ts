4bfe386e332952119863949fac0718eead193d91
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-26 13:50:45 +0100

    :heavy_minus_sign: Remove metascraper packages

--- helpers/meta.ts
+++ helpers/meta.ts
@@ -1,28 +1,5 @@
 import got from "got";
-import metascraper from "metascraper";
-import author from "metascraper-author";
-import date from "metascraper-date";
-import description from "metascraper-description";
-import publisher from "metascraper-publisher";
-import title from "metascraper-title";
-import url from "metascraper-url";
-import lang from "metascraper-lang";
-
-const scraper = metascraper([
-  author(),
-  date(),
-  description(),
-  publisher(),
-  title(),
-  url(),
-  lang()
-]);
 
 export const scrapeMetaTags = async (targetUrl: string) => {
-  const { body }: { body: { html: string; url: string } } = await got(
-    targetUrl
-  );
-  const { html, url } = body;
-  const metadata = await scraper({ html, url });
-  return metadata;
+  return targetUrl;
 };
