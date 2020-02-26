9f1e109f83f08b22a15310d6eae9a78e6579ea1f
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-26 14:08:35 +0100

    :wrench: Add Elsevier API key

--- helpers/meta.ts
+++ helpers/meta.ts
@@ -1,5 +1,40 @@
 import got from "got";
+import { load } from "cheerio";
+
+const value = ($: CheerioStatic, name: string) =>
+  $(`meta[name="citation_${name}"]`).attr("content");
 
 export const scrapeMetaTags = async (targetUrl: string) => {
-  return targetUrl;
+  const { body: html } = await got.get(targetUrl);
+  const $ = load(html);
+  const pii = value($, "pii");
+  const issn = value($, "issn");
+  const volume = value($, "volume");
+  const lastpage = value($, "lastpage");
+  const publisher = value($, "publisher");
+  const firstpage = value($, "firstpage");
+  const journal_title = value($, "journal_title");
+  const type = value($, "type");
+  const doi = value($, "doi");
+  const article_type = value($, "article_type");
+  const title = value($, "title");
+  const publication_date = value($, "publication_date");
+  const online_date = value($, "online_date");
+  const pdf_url = value($, "pdf_url");
+  return {
+    pii,
+    issn,
+    volume,
+    lastpage,
+    publisher,
+    firstpage,
+    journal_title,
+    type,
+    doi,
+    article_type,
+    title,
+    publication_date,
+    online_date,
+    pdf_url
+  };
 };
